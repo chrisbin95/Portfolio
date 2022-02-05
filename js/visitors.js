3fc8a78304fafe88bf959e40edc1c55e6e087d3d
Chrisbin Thomas <80977531+chrisbin95@users.noreply.github.com>
2022-02-05 14:03:28 +0530

    visitors count

---
+++ js/visitors.js
@@ -0,0 +1,21 @@
+
+/*To Create CountAPI*/
+/*https://api.countapi.xyz/create?namespace=localhost7001&key=visits&value=0*/
+/*To hit CountAPI*/
+/*https://api.countapi.xyz/hit/localhost7001/visits*/
+/*To get CountAPI*/
+/*https://api.countapi.xyz/get/localhost7001/visits*/
+/*To update CountAPI*/
+/*https://api.countapi.xyz/update/localhost7001/visits/?amount=1*/
+
+const countEl = document.getElementById('CounterVisitor');
+
+updateVisitCount();
+
+function updateVisitCount() {
+	fetch('https://api.countapi.xyz/update/localhost7001/visits/?amount=1')
+	.then(res => res.json())
+	.then(res => {
+		countEl.innerHTML = res.value;
+	})
+}
